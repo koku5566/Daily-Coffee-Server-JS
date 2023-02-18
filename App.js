18cb9b5b853ef6626f716fc434bb251f91ea5e00
koku5566 <koku5566@gmail.com>
2023-02-18 12:42:59 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,29 +1,41 @@
-//import express from 'express';
-const express = require('express');
+////import express from 'express';
+//const express = require('express');
+//
+////import sequelize from './utils/db.js';
+//const sequelize = require('./server/utils/db');
+//
+////import router from './routes/routes.js';
+//const router = require('./server/routes/routes')
+//
+//const app = express();
+//
+//app.use(express.urlencoded({ extended: true }));
+//
+//app.use(express.json());
+//
+//app.use((req, res, next) => {
+//    res.setHeader('Access-Control-Allow-Origin', '*');
+//    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+//    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//    next();
+//});
+//
+//
+//app.use('/api',router);
+//console.log(router);
+//
+//sequelize.sync()
+//
+//app.listen(5000);
 
-//import sequelize from './utils/db.js';
-const sequelize = require('./server/utils/db');
+const express = require('express')
+const app = express()
+const port = 3000
 
-//import router from './routes/routes.js';
-const router = require('./server/routes/routes')
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.json());
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
-
-app.use('/api',router);
-console.log(router);
-
-sequelize.sync()
-
-app.listen(5000);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
