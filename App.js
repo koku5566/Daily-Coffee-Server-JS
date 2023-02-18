909a831e291be1a5fbbcba15d0e9ae40232f9a9d
koku5566 <koku5566@gmail.com>
2023-02-18 11:13:30 +0800

    change export module

--- App.js
+++ App.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
 
 
 app.use(router);
+console.log(router);
 
 sequelize.sync()
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,16 +3,17 @@
 //import express from 'express';
 const express = require('express');
 
-const { signup, login, isAuth } = require('../controller/auth');
+//const { signup, login, isAuth } = require('../controller/auth');
+const auth = require('../controller/auth');
 
 
 const router = express.Router();
 
-router.post('/login', login);
+router.post('/login', auth.login);
 
-router.post('/signup', signup);
+router.post('/signup', auth.signup);
 
-router.get('/private', isAuth);
+router.get('/private', auth.isAuth);
 
 router.get('/public', (req,res,next) =>{
     res.status(200).json({message: "Here is your public resource" });
