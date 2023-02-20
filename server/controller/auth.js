1e3248a3698224fbb1cbac271f475afc52ce2a1f
koku5566 <koku5566@gmail.com>
2023-02-20 12:24:17 +0800

    Update auth.js

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -92,7 +92,7 @@ const login = (req, res, next) => {
 const isAuth = (req, res, next) => {
     const authHeader = req.get("Authorization");
     if (!authHeader) {
-        return res.status(401).json({ message: 'not authenticated' });
+        return res.status(200).json({ message: 'not authenticated' });
     };
     const token = authHeader.split(' ')[1];
     let decodedToken;
