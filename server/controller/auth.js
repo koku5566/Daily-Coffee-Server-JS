0dc38e253a6ec78062a037cee5de94b1dd3eddd5
koku5566 <koku5566@gmail.com>
2023-02-20 11:57:14 +0800

    test signup api

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -7,49 +7,54 @@ const jwt = require('jsonwebtoken');
 //const JsonWebTokenError =require('jsonwebtoken');
 const User = require('../model/user');
 
+// const signup = (req, res, next) => {
+//     //Check if email already exists
+//     User.findOne({
+//         where: {
+//             email: req.body.email,
+//         }
+//     })
+//         .then(dbUser => {
+//             if (dbUser) {
+//                 return res.status(409).json({ message: "Email Exists" });
+//             }
+//             else if (req.body.email && req.body.password) {
+//                 //password hash
+//                 bcrypt.hash(req.body.password, 12, (err, passwordHash) => {
+//                     if (err) {
+//                         return res.status(500).json({ message: "Couldnt hash the password" });
+//                     }
+//                     else if (passwordHash) {
+//                         return User.create(({
+//                             email: req.body.email,
+//                             name: req.body.name,
+//                             password: passwordHash,
+//                         }))
+//                             .then(() => {
+//                                 res.status(200).json({ message: "User Created" });
+//                             })
+//                             .catch(err => {
+//                                 console.log(err);
+//                                 res.status(502).json({ message: "error while creating the user" });
+//                             });
+//                     };
+//                 });
+//             }
+//             else if (!req.body.password) {
+//                 return res.status(400).json({ message: "Password not provided" });
+//             }
+//             else if (!req.body.email) {
+//                 return res.status(400).json({ message: "Email not provided" });
+//             };
+//         })
+//         .catch(err => {
+//             console.log('error', err);
+//         });
+// };
+
+//test
 const signup = (req, res, next) => {
-    //Check if email already exists
-    User.findOne({
-        where: {
-            email: req.body.email,
-        }
-    })
-        .then(dbUser => {
-            if (dbUser) {
-                return res.status(409).json({ message: "Email Exists" });
-            }
-            else if (req.body.email && req.body.password) {
-                //password hash
-                bcrypt.hash(req.body.password, 12, (err, passwordHash) => {
-                    if (err) {
-                        return res.status(500).json({ message: "Couldnt hash the password" });
-                    }
-                    else if (passwordHash) {
-                        return User.create(({
-                            email: req.body.email,
-                            name: req.body.name,
-                            password: passwordHash,
-                        }))
-                            .then(() => {
-                                res.status(200).json({ message: "User Created" });
-                            })
-                            .catch(err => {
-                                console.log(err);
-                                res.status(502).json({ message: "error while creating the user" });
-                            });
-                    };
-                });
-            }
-            else if (!req.body.password) {
-                return res.status(400).json({ message: "Password not provided" });
-            }
-            else if (!req.body.email) {
-                return res.status(400).json({ message: "Email not provided" });
-            };
-        })
-        .catch(err => {
-            console.log('error', err);
-        });
+    return res.status(200).json({message: "Sign up can work" });
 };
 
 const login = (req, res, next) => {
