fc31583e4c8ebf066efd2ee74c235f9f9a29ec77
koku5566 <koku5566@gmail.com>
2023-02-20 12:43:30 +0800

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -24,7 +24,7 @@ router.get('/public', (req,res,next) =>{
 //});
 
 router.get('/test', (req,res,next) => {
-    res.status(405).json({error : "page not found"});
+    res.status(405).json({message : "page not found"});
 });
 
 module.exports = router;
